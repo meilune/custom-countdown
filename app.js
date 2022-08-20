005c36850232f64fea3b669c9a9489d0b81e2311
Ginja <virginija.meilune@gmail.com>
2022-08-20 16:10:02 +1000

    Commiting first set of JS settings

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const newTitle = document.getElementById("title");
 const newDate = document.getElementById("date-picker");
+const countdownForm = document.getElementById("countdownForm");
 const setDateContainer = document.getElementById("input-container");
 const showCountdownContainer = document.getElementById("countdown");
 const completeContainer = document.getElementById("complete");
 
-
-
+let today = new Date().toISOString().split("T")[0];
+console.log(today);
+newDate.setAttribute('min', today);
