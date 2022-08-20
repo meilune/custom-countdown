b0ac4fe7011e27041eb387c792e65625b361199f
Ginja <virginija.meilune@gmail.com>
2022-08-20 15:37:06 +1000

    Adding final UI changes

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Testing")
+const newTitle = document.getElementById("title");
+const newDate = document.getElementById("date-picker");
+const setDateContainer = document.getElementById("input-container");
+const showCountdownContainer = document.getElementById("countdown");
+const completeContainer = document.getElementById("complete");
+
+
+
