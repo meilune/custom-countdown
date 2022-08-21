54ceba864278f427960679d8191a3eb6448251f6
Ginja <virginija.meilune@gmail.com>
2022-08-21 21:30:48 +1000

    Adding finishing touches

--- app.js
+++ app.js
@@ -14,15 +14,31 @@ let countdownTitle = "";
 let endDate = "";
 let countdownValue = Date;
 
+//Setting variable for the countdown object to be saved in the LocalServer
 let savedCountdown = {};
 
 //Set the calendar min date so that the earlier date can not be chosen
 let today = new Date().toISOString().split("T")[0];
 newDate.setAttribute('min', today);
 
+//Function that clears the interval so it can be restarted again
+function myStopFunction() {
+    clearInterval();
+    }
+
+//Function that updates the DOM with information for countdown or complete templates
 function updateDOM() {
+    //get the time value of the current date
     const now = new Date().getTime();
+    //Calculate the amount of time between due date and current date
     const distance = countdownValue - now;
+
+    //Display Complete template if the countdown finished
+    if(distance <= 0) {
+        showCountdownContainer.style.display = "none";
+        completeContainer.style.display = "block";
+        completeInfo.textContent = `${countdownTitle} finished on ${endDate}`;
+    } else {
     //Setting variables for countdown times
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -37,17 +53,14 @@ function updateDOM() {
     //Displaying the template
     setDateContainer.style.display = "none";
     showCountdownContainer.style.display = "block";
-    //Display Complete template if the countdown finished
-    if(distance <= 0) {
-        showCountdownContainer.style.display = "none";
-        completeContainer.style.display = "block";
-        completeInfo.textContent = `${countdownTitle} finished on ${endDate}`;
     }
 }
 
 //Take Values from the Input
 function updateCountdown(e) {
+    //Prevent page from refreshing when the button is clicked
     e.preventDefault();
+    //Source the values from the event
     countdownTitle = e.srcElement[0].value;
     endDate = e.srcElement[1].value;
 
@@ -56,6 +69,7 @@ function updateCountdown(e) {
         title: countdownTitle,
         date: endDate
     }
+    //Saving the items in object format
     localStorage.setItem("countdown", JSON.stringify(savedCountdown));
 
     //Check if the date is selected, else get the number of the date
@@ -64,11 +78,9 @@ function updateCountdown(e) {
     } else {
         countdownValue = new Date(endDate).getTime();
 
-        //Setting the interval so that the countdown works:
+        //Setting the interval so that the countdown moves backwards:
         const myInterval = setInterval(updateDOM, 1000);
-        function myStopFunction() {
-        clearInterval(myInterval);
-        }
+        myStopFunction(myInterval);
     } 
 }
 
@@ -81,16 +93,18 @@ function refreshPage(){
 function restorePrevCount() {
     //Get countdown from Local Storage if available
     if(localStorage.getItem("countdown")) {
+        //If there is an existing countdown, show the respective template
         countdownForm.style.display = "none";
+        //Getting items from the storage
         savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+        //Assigning values from the stored object
         countdownTitle = savedCountdown.title;
         endDate = savedCountdown.date;
         showCountdownContainer.style.display = "block";
+        //Loading the function so that countdown works
         countdownValue = new Date(endDate).getTime();
         const myInterval = setInterval(updateDOM, 1000);
-        function myStopFunction() {
-        clearInterval(myInterval);
-        } 
+        myStopFunction(myInterval);
     }
 }
 
