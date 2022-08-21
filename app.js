a8c5e6056bab54a39239f6f6eff7ab8f8d6e60ed
Ginja <virginija.meilune@gmail.com>
2022-08-21 18:55:58 +1000

    Adding functionality for displaying the Countdown Complete page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const showCountdownContainer = document.getElementById("countdown");
 const completeContainer = document.getElementById("complete");
 const countdownBtn = document.getElementById("countdown-button");
 const timeElements = document.querySelectorAll("span");
+const completeBtn = document.getElementById("complete-button");
 
 //Set the global variables
 let countdownTitle = "";
@@ -19,17 +20,26 @@ newDate.setAttribute('min', today);
 function updateDOM() {
     const now = new Date().getTime();
     const distance = countdownValue - now;
+    //Setting variables for countdown times
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //Adding values to the existing template
     timeElements[0].textContent = days;
     timeElements[1].textContent = hours;
     timeElements[2].textContent = minutes;
     timeElements[3].textContent = seconds;
     newTitle.textContent = countdownTitle;
+    //Displaying the template
     setDateContainer.style.display = "none";
     showCountdownContainer.style.display = "block";
+    //Display Complete template if the countdown finished
+    if(distance === 0 || distance < 0) {
+        showCountdownContainer.style.display = "none";
+        completeContainer.style.display = "block";
+        timeElements[4].textContent = endDate;
+    }
 }
 
 //Take Values from the Input
@@ -37,15 +47,23 @@ function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     endDate = e.srcElement[1].value;
-    console.log(countdownTitle, endDate);
-    //Get number version of current date, update DOM
     countdownValue = new Date(endDate).getTime();
-    console.log(countdownValue); 
-    updateDOM();
 
+    //Setting the interval so that the countdown works:
+    const myInterval = setInterval(updateDOM, 1000);
+    function myStopFunction() {
+    clearInterval(myInterval);
+    }
 }
+
+//Refresh page when resetting the countdown
+function refreshPage(){
+    window.location.reload();
+} 
+
 
 //Event Listeners
 countdownForm.addEventListener('submit', updateCountdown);
-
+countdownBtn.addEventListener('click', refreshPage);
+completeBtn.addEventListener('click', refreshPage);
 
